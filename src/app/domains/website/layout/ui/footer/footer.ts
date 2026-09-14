fc0972e71a910a3b8ccea25cea2719cdb7f0021b
Wilfried Musanzi <musanziw.m@gmail.com>
2026-09-14 15:39:27 +0200

    feat: refactor footer navigation to use dynamic links from NAVIGATION_LINKS

--- src/app/domains/website/layout/ui/footer/footer.ts
+++ src/app/domains/website/layout/ui/footer/footer.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import { MatIconModule } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
+import { NAVIGATION_LINKS } from '../../data/navigation';
 
 @Component({
   selector: 'website-footer',
@@ -8,5 +9,6 @@ import { RouterLink } from '@angular/router';
   templateUrl: './footer.html'
 })
 export class WebsiteFooter {
+  protected readonly links = NAVIGATION_LINKS;
   protected readonly currentYear = new Date().getFullYear();
 }
